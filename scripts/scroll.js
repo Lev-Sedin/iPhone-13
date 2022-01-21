28ac9bba40c853fed46c12782b7f947304b889dc
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-01-21 23:46:18 +0300

    Add files via upload

---
+++ scripts/scroll.js
@@ -0,0 +1,38 @@
+const links = document.querySelectorAll(".header-menu__item a")
+const dop = document.querySelector(".card-details__link-characteristics")
+
+
+seamless.polyfill();
+
+links.forEach( (element) => {
+    element.addEventListener("click", (event) =>{
+        event.preventDefault()
+        const id = element.getAttribute("href").substring(1)
+        const section = document.getElementById(id)
+        console.log(section);
+
+        if(section){
+            seamless.elementScrollIntoView(section, {
+                behavior: "smooth",
+                block: "start"
+            })
+        } else{
+            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                behavior: "smooth",
+                block: "center",
+                inline: "center",
+            });
+        }
+    })
+})
+
+dop.addEventListener("click", (event) =>{
+    event.preventDefault()
+    const id = dop.getAttribute("href").substring(1)
+    if(document.getElementById('characteristics')) {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+            behavior: "smooth",
+            block: "start",
+        })
+    }  
+})
