7e262e712813723fdd79756722512cd9074481ac
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-01-25 01:36:00 +0300

    Add files via upload

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,38 +1,41 @@
-const links = document.querySelectorAll(".header-menu__item a")
-const dop = document.querySelector(".card-details__link-characteristics")
+const scrollFunc = () =>{
+    const links = document.querySelectorAll(".header-menu__item a")
+    const dop = document.querySelector(".card-details__link-characteristics")
 
 
-seamless.polyfill();
+    seamless.polyfill();
 
-links.forEach( (element) => {
-    element.addEventListener("click", (event) =>{
+    links.forEach( (element) => {
+        element.addEventListener("click", (event) =>{
+            event.preventDefault()
+            const id = element.getAttribute("href").substring(1)
+            const section = document.getElementById(id)
+            console.log(section);
+
+            if(section){
+                seamless.elementScrollIntoView(section, {
+                    behavior: "smooth",
+                    block: "start"
+                })
+            } else{
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
+        })
+    })
+
+    dop.addEventListener("click", (event) =>{
         event.preventDefault()
-        const id = element.getAttribute("href").substring(1)
-        const section = document.getElementById(id)
-        console.log(section);
-
-        if(section){
-            seamless.elementScrollIntoView(section, {
-                behavior: "smooth",
-                block: "start"
-            })
-        } else{
+        const id = dop.getAttribute("href").substring(1)
+        if(document.getElementById('characteristics')) {
             seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
                 behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
+                block: "start",
+            })
+        }  
     })
-})
-
-dop.addEventListener("click", (event) =>{
-    event.preventDefault()
-    const id = dop.getAttribute("href").substring(1)
-    if(document.getElementById('characteristics')) {
-        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-            behavior: "smooth",
-            block: "start",
-        })
-    }  
-})
+}
+scrollFunc()
